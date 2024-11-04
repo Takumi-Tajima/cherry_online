b7f841566a5ac7152852f37003dc853c8ce53d23
taji <taji@sonicgarden.jp>
2024-11-04 16:07:40 +0900

    バリデーション追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates :released_on, comparison: { greater_than: Time.current }
+
+  scope :default_order, -> { order(created_at: :desc, id: :desc) }
 end
