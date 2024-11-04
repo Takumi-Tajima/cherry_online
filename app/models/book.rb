e32eff0c968c8df1bad116579863cc5c092506ce
taji <taji@sonicgarden.jp>
2024-11-05 08:25:22 +0900

    cart_itemsのリレーションを組む

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+
   validates :title, presence: true
   validates :price, numericality: { only_integer: true }
   validates :released_on, comparison: { greater_than: Time.current }
