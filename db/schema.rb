27db32f78f734a46a91f82945bb7574f6fd73921
taji <taji@sonicgarden.jp>
2024-11-04 16:55:01 +0900

    Userを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_04_070013) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_04_075019) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,5 +29,15 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_04_070013) do
     t.date "released_on"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
