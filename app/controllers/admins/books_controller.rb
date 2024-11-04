30010d79b583ad922bb3ae229e95a36337abf5df
taji <taji@sonicgarden.jp>
2024-11-04 16:12:28 +0900

    Admin配下にbooksを追加

--- app/controllers/admins/books_controller.rb
+++ app/controllers/admins/books_controller.rb
@@ -1,4 +1,4 @@
-class BooksController < ApplicationController
+class Admins::BooksController < Admins::ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
 
   def index
