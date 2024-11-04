d375e3f886b7f3c30cd8ec1e6dd33701645f4b23
taji <taji@sonicgarden.jp>
2024-11-05 08:44:20 +0900

    カート1つに対して同じ本は1つのみ追加可能であるバリデーションを追加

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,5 +2,7 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :book
 
+  validates :book_id, uniqueness: { scope: :cart_id }
+
   scope :default_order, -> { order(created_at: :desc, id: :desc) }
 end
