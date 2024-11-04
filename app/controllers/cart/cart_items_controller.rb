eb1d3e2303cd37a5807c493d9db2841316b0fc50
taji <taji@sonicgarden.jp>
2024-11-05 08:29:12 +0900

    cart_items_controllerを追加する

---
+++ app/controllers/cart/cart_items_controller.rb
@@ -0,0 +1,12 @@
+class Cart::CartItemsController < ApplicationController
+  def create
+    book = Book.find(params[:book_id])
+    current_cart.cart_items.find_or_create_by!(book:)
+    redirect_to books_path, notice: 'カートに追加しました'
+  end
+
+  def destroy
+    current_cart.cart_items.find(params[:id]).destroy!
+    redirect_to cart_path, notice: 'カートから削除しました'
+  end
+end
