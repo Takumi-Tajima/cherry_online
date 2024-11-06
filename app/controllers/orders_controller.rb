2aa2c33bc33e15b5f95c26870592bfe4fec70f58
taji <taji@sonicgarden.jp>
2024-11-06 18:10:29 +0900

    注文機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
   def create
+    order = current_user.orders.create!(purchased_at: Time.current)
+    order.order_items_create_from_cart(current_cart)
+    redirect_to books_path
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_items, dependent: :destroy
+
+  def order_items_create_from_cart(current_cart)
+    transaction do
+      current_cart.cart_items.each do |cart_item|
+        order_items.build(book: cart_item.book, price: cart_item.book.price)
+      end
+      save!
+      current_cart.destroy!
+    end
+  end
 end
