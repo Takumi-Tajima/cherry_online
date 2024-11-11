d063bb3edd1eb7133b5fe6d3c0dcef397491bcba
taji <taji@sonicgarden.jp>
2024-11-11 18:57:12 +0900

    orders_controllerの継承元をUsers::ApplicationControllerに修正

--- app/controllers/users/orders_controller.rb
+++ app/controllers/users/orders_controller.rb
@@ -1,4 +1,4 @@
-class Users::OrdersController < ApplicationController
+class Users::OrdersController < Users::ApplicationController
   def index
     @orders = current_user.orders.default_order
   end
